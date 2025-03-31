ecd373ced6b01711da41f1019fdceb499b4de7f2
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-03-31 20:30:21 +0700

    grid

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const navigation = document.getElementById("navigation");
 
 let isNowTasks = true;
 
+
 let tasks = [
   {
     date: {
@@ -80,23 +81,23 @@ const render = () => {
 
   navigation.innerHTML = `
     <div class="flex text-sm gap-2">
-        <button onclick="changeList(${true})" class=" w-1/2 py-1 font-bold duration-300 ${
+        <button onclick="changeList(${true})" class=" lg:text-xl w-1/2 py-1 font-bold duration-300 ${
     isNowTasks ? "text-black bg-white" : "border  hover:bg-white/30"
   } rounded cursor-pointer">Tasks</button>
-        <button onclick="changeList(${false})"  class=" w-1/2 py-1 font-bold duration-300 ${
+        <button onclick="changeList(${false})"  class=" lg:text-xl w-1/2 py-1 font-bold duration-300 ${
     !isNowTasks
       ? "text-black bg-white"
       : "border  hover:bg-white/30"
   } rounded cursor-pointer">Completed Tasks</button>
     </div>
-    <button onclick="handleDeleteAll()" class="  bg-red-500  cursor-pointer  font-bold px-4 p-1 rounded hover:bg-red-600 active:bg-red-700 ">DELETE ALL <i class="fa-solid fa-trash fa-lg ml-2 "></i></button>
+    <button onclick="handleDeleteAll()" class=" lg:text-xl  bg-gray-500  cursor-pointer  font-bold px-4 p-1 rounded hover:bg-gray-600 active:bg-gray-700  lg:ml-auto hover:text-red-600 ">DELETE ALL <i class="fa-solid fa-trash fa-lg ml-2 "></i></button>
     `;
 
   if (!currentLists.length) {
     root.innerHTML = `
-        <p class="text-center content-center min-h-72 font-bold text-2xl">${
+        <p class="text-center content-center min-h-72 font-bold text-2xl text-white border-2">${
           isNowTasks
-            ? "Tidak ada tasks tersedia 😴"
+            ? "Kamu belum membuat task apapun 😴"
             : "Tidak ada tasks yang sudah selesai 😴"
         }</p>
         `;
@@ -108,49 +109,38 @@ const render = () => {
       if (task.priority == "Medium") colorPriority = "bg-orange-500/70";
       if (task.priority == "High") colorPriority = "bg-red-500/70";
       innerRoot += `
-            <div class="border text-sm py-2 px-4 flex gap-2 flex-col  ${task.done ? "bg-zinc-400" : "odd:bg-zinc-200 even:bg-zinc-300"} rounded  ">
-                <section class=" ">
+            <div class="border text-sm py-2 px-4 ${task.done ? "bg-zinc-400" : "odd:bg-zinc-200 even:bg-zinc-300"} rounded lg:hover:scale-105 hover:bg-zinc-400 duration-300 relative lg:text-base">
+              <div class="space-y-4 mb-4 ">
+                  <section class="">
                     <p class="text-end mb-2">${task.date.day}, ${task.date.full}</p>
                     <div class="flex gap-10 ">
-                        <p class="font-semibold"><span class="font-bold">Name</span> : ${
-                          task.name
-                        }</p>
-                        <p class="font-semibold"><span class="font-bold">Position</span> : ${
-                          task.position
-                        }</p>
+                        <p class="font-semibold"><span class="font-bold">Name</span> : ${task.name}</p>
+                        <p class="font-semibold"><span class="font-bold">Position</span> : ${task.position}</p>
                     </div>
-                </section>
-                <section class="flex items-center justify-between gap-4">
-                    <p class="font-bold w-max">Priority : <span class="px-3 py-1 rounded   ${colorPriority} text-white">${
-        task.priority
-      }</span></p> 
+                  </section>
+                  <section class="flex items-center justify-between gap-4 ">
+                    <p class="font-bold w-max">Priority : <span class="px-3 py-1 rounded   ${colorPriority} text-white">${task.priority}</span></p> 
                     <div class="flex items-center">
-                    ${
-                      isNowTasks
-                        ? `
-                    <label for="done" class=" font-semibold">
+                    ${isNowTasks? `
+                    <label for="done_${index}" class=" font-semibold">
                         Done 
                     </label>
-                    <input class="size-5 mr-5 ml-1 " ${
-                      task.done && "checked"
-                    } type="checkbox" name="done" id="done" onclick="handleDoneToggle(${index})">
+                    <input class="size-5 mr-5 ml-1 " ${task.done && "checked"} type="checkbox" name="done" id="done_${index}"  onclick="handleDoneToggle(${index})">
                         `
                         : ""
                     }
-                   ${
-                     isNowTasks
+                   ${isNowTasks
                        ? `<button onclick="handleDelete(${index})" class="cursor-pointer group text-red-500 hover:text-red-600 active:text-red-700"><i class="fa-solid fa-trash fa-lg "></i></button>`
                        : ""
                    }
-                    </div>
-                    
-                    
-                </section>            
-                <section class="text-zinc-600 font-semibold ">
-                    <p class="font-bold">Task : <br /><span class=" ${
+                      </div>                 
+                  </section>  
+              </div>          
+              <section class="text-zinc-600 font-semibold ">
+                    <p class="font-bold">Task : <br /><span class="text-gray-700 ${
                       task.done && "line-through"
                     }">${task.task}</span></p>
-                </section>
+              </section>
             </div>
             `;
     });
@@ -179,7 +169,10 @@ const handleDelete = (index) => {
 };
 
 const handleDeleteAll = () => {
+  const confirm = window.confirm("Are you sure to delete all tasks?")
+  if(!confirm) return 
   tasks = [];
+  completedTasks = tasks.filter((a) => a.done);
   render();
 };
 
